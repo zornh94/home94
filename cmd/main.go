50be369368ec52e213c233ca3732e25cc560aeff
tndevr <home@jetfuel.com>
2020-03-28 22:48:01 +0800

    v1

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/tndevr/packages"
-//	"net/http"
 )
-/*
-func main(){
-	r := gin.Default()
-	r.GET("/",func (ctx *gin.Context){
-		ctx.String(http.StatusOK, "hello gin"
-	})
-	fmt.Println("Server listen Default 8080")
-	r.Run()
-}*/
 
 func main(){
 	r := gin.Default()
--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.6.2 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
-	github.com/tndevr/packages v0.0.2
+	github.com/tndevr/packages v1.0.0
 )
+
+
